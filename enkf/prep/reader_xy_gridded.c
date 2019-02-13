a499e3ed4b12360a1067f05c92d095b54bffa70c
Hugo Oliveira <ocehugo@gmail.com>
2019-02-13 16:21:08 +1100

    add support to record dimension == 1

--- enkf/prep/reader_xy_gridded.c
+++ enkf/prep/reader_xy_gridded.c
@@ -170,7 +170,10 @@ void reader_xy_gridded(char* fname, int fid, obsmeta* meta, grid* g, observation
     ncw_inq_vardims(ncid, varid_var, 3, &ndim_var, dimlen_var);
     if (ndim_var == 3) {
         if (!ncw_var_hasunlimdim(ncid, varid_var))
+            /* No need for this 
             enkf_quit("reader_xy_gridded(): %s: %s: depends on 3 dimensions, but has no unlimited dimension", fname, varname);
+            */
+            ;
         if (dimlen_var[0] != 1)
             enkf_quit("reader_xy_gridded(): %s: %s: %d records (currently only one is allowed)", fname, varname, dimlen_var[0]);
         n_var = dimlen_var[1] * dimlen_var[2];
